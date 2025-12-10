56a892045df712771db3c81058517d2440a4753d
UBornheimer <u.bornheimer@gmail.com>
2025-12-10 21:36:34 +0800

    remove loom.__revision__
    Update version to v0.3.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "el-loom"
 copyright = "2025, Entropica Labs Pte Ltd"
 author = "Entropica Labs Pte Ltd"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
